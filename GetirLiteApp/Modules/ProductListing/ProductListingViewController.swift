e77f6fd47974a9aa6380c9fdd572968720958042
GradByte <eertekin99@outlook.com>
2024-04-16 20:32:24 +0200

    Listing page compositional layout

--- GetirLiteApp/Modules/ProductListing/ProductListingViewController.swift
+++ GetirLiteApp/Modules/ProductListing/ProductListingViewController.swift
@@ -11,11 +11,10 @@ import Kingfisher
 
 final class ProductListingViewController: UIViewController, ProductListingViewControllerProtocol {
     
-    private var horizontalCollectionView: UICollectionView!
-    private var verticalCollectionView: UICollectionView!
-    
-    private let horizontalCellIdentifier = "productCell"
-    private let verticalCellIdentifier = "productCell"
+    private var collectionView: UICollectionView!
+    //    private var horizontalCollectionView: UICollectionView!
+    //    private var verticalCollectionView: UICollectionView!
+    private let productCellIdentifier = "productCell"
     
     var suggestedProducts = [Product]()
     var mainProducts = [ProductElement]()
@@ -41,26 +40,28 @@ final class ProductListingViewController: UIViewController, ProductListingViewCo
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         presenter.viewDidLoad(view: self)
         
-        setupUI()
-        //NetworkingManager.shared.fetchMainProducts()
-        //NetworkingManager.shared.fetchSuggestedProducts()
         setupNavigationBar()
-        
-        setupHorizontalCollectionView()
-        setupVerticalCollectionView()
-        
+        setupUI()
+        fetchData()
+    }
+}
+
+// MARK: - Setup UI Elements
+extension ProductListingViewController {
+    private func setupUI() {
+        setupCollectionView()
+    }
+    
+    private func fetchData() {
         NetworkingManager.shared.fetchSuggestedProducts { products in
             DispatchQueue.main.async {
                 if let products = products {
-                    // Handle the fetched products here
                     self.suggestedProducts = products
-                    self.horizontalCollectionView.reloadData()
-                    // print(products)
+                    //                    self.horizontalCollectionView.reloadData()
+                    self.collectionView.reloadData()
                 } else {
-                    // Handle the case where products couldn't be fetched
                     print("Failed to fetch products")
                 }
             }
@@ -69,90 +70,99 @@ final class ProductListingViewController: UIViewController, ProductListingViewCo
         NetworkingManager.shared.fetchMainProducts { products in
             DispatchQueue.main.async {
                 if let products = products {
-                    // Handle the fetched products here
                     self.mainProducts = products
-                    self.verticalCollectionView.reloadData()
-                    // print(products)
+                    //                    self.verticalCollectionView.reloadData()
+                    self.collectionView.reloadData()
                 } else {
-                    // Handle the case where products couldn't be fetched
                     print("Failed to fetch products")
                 }
             }
         }
     }
-}
-
-// MARK: - Setup UI Elements
-extension ProductListingViewController {
-    private func setupUI() {
-        view.backgroundColor = GetirColor.almostWhiteGray
-        setupHorizontalCollectionView()
-        setupHorizontalCollectionView()
-    }
     
-    private func setupHorizontalCollectionView() {
-        // Create a layout for the horizontal collection view
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        
-        // Set the item size to fit one item per column
-        layout.itemSize = CGSize(width: 100, height: 120) // Adjust width and height as needed
-        
-        // Initialize the horizontal collection view with the custom layout
-        horizontalCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        horizontalCollectionView.backgroundColor = .white
-        horizontalCollectionView.register(ProductCell.self, forCellWithReuseIdentifier: horizontalCellIdentifier)
-        horizontalCollectionView.dataSource = self
-        view.addSubview(horizontalCollectionView)
-        
-        // Enable horizontal scrolling
-        horizontalCollectionView.isScrollEnabled = true
-        
-        // Add constraints for horizontal collection view
-        horizontalCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            horizontalCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            horizontalCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            horizontalCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            horizontalCollectionView.heightAnchor.constraint(equalToConstant: 140) // Adjust height as needed
-        ])
-    }
-    
-    private func setupVerticalCollectionView() {
-        // Calculate the width of each item based on the available space and the number of items per row
-        let itemWidth = (view.bounds.width - 30) / 3  // Subtracting 30 to account for spacing between items
-        
-        // Add the vertical collection view
-        let verticalLayout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
-            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(itemWidth), heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            
-            // Set the spacing between items
-            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
-            
-            // Calculate the group size based on the width of each item and the number of items per row
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(150))
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)  // 3 items per row
-            
-            let section = NSCollectionLayoutSection(group: group)
-            section.interGroupSpacing = 10
-            return section
+    private func setupCollectionView() {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, environment in
+            switch sectionIndex {
+            case 0:
+                return self.createHorizontalSectionLayout()
+            case 1:
+                return self.createVerticalSectionLayout()
+            default:
+                return self.createDefaultSectionLayout()
+            }
         }
         
-        verticalCollectionView = UICollectionView(frame: .zero, collectionViewLayout: verticalLayout)
-        verticalCollectionView.backgroundColor = .white
-        verticalCollectionView.register(ProductCell.self, forCellWithReuseIdentifier: verticalCellIdentifier)
-        verticalCollectionView.dataSource = self
-        view.addSubview(verticalCollectionView)
+        layout.register(BackgroundDecorationView.self, forDecorationViewOfKind: "background")
         
-        // Add constraints for vertical collection view
-        verticalCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ProductCell.self, forCellWithReuseIdentifier: productCellIdentifier)
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            verticalCollectionView.topAnchor.constraint(equalTo: horizontalCollectionView.bottomAnchor, constant: 20),
-            verticalCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            verticalCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            verticalCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        collectionView.backgroundColor = GetirColor.almostWhiteGray
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+    }
+    
+    private func createHorizontalSectionLayout() -> NSCollectionLayoutSection {
+        // Define item size and group size for horizontal section
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(150))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        
+        // Ensure horizontal scrolling
+        section.orthogonalScrollingBehavior = .continuous
+        
+        let backgroundDecoration = NSCollectionLayoutDecorationItem.background(elementKind: "background")
+        backgroundDecoration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+        section.decorationItems = [backgroundDecoration]
+        
+        return section
+    }
+    
+    private func createVerticalSectionLayout() -> NSCollectionLayoutSection {
+        // Define item size and group size for vertical section
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(150))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        
+        let backgroundDecoration = NSCollectionLayoutDecorationItem.background(elementKind: "background")
+        backgroundDecoration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+        section.decorationItems = [backgroundDecoration]
+        
+        return section
+    }
+    
+    private func createDefaultSectionLayout() -> NSCollectionLayoutSection {
+        // Define a simple layout for default section (fallback)
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(150))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: itemSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
     }
     
     private func setupNavigationBar() {
@@ -219,54 +229,60 @@ extension ProductListingViewController {
     }
 }
 
+// MARK: - UICollectionView Data Source
 extension ProductListingViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        if collectionView == horizontalCollectionView {
-            return suggestedProducts.count
-        } else {
+        if section == 0 {
+            return suggestedProducts.count // Horizontal layout
+        } else if section == 1 {
             return mainProducts.count
+        } else {
+            return 0 // Default section has no items
         }
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == horizontalCollectionView {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: horizontalCellIdentifier, for: indexPath) as! ProductCell
-            
-            let currentProduct = suggestedProducts[indexPath.row]
-            
-            // Convert the imageURL string to URL
-            if let imageURL = URL(string: (currentProduct.imageURL ?? currentProduct.squareThumbnailURL) ?? "") {
-                // Configure the cell with Kingfisher
-                cell.configure(with: imageURL, price: currentProduct.priceText ?? "", name: currentProduct.name ?? "", attribute: currentProduct.shortDescription ?? "")
-            } else {
-                // Set a placeholder image if the imageURL string is invalid
-                cell.configure(with: nil, price: currentProduct.priceText ?? "", name: currentProduct.name ?? "", attribute: currentProduct.shortDescription ?? "")
-            }
-            
-            
-            return cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: productCellIdentifier, for: indexPath) as! ProductCell
+        
+        if indexPath.section == 0 {
+            let currentProduct = suggestedProducts[indexPath.item]
+            configureCellProduct(cell, with: currentProduct)
+        } else if indexPath.section == 1 {
+            let currentProduct = mainProducts[indexPath.item]
+            configureCellProductElement(cell, with: currentProduct)
+        }
+        
+        return cell
+    }
+    
+    private func configureCellProduct(_ cell: ProductCell, with product: Product) {
+        if let imageURL = URL(string: (product.imageURL ?? product.squareThumbnailURL) ?? "") {
+            cell.configure(with: imageURL, price: product.priceText ?? "", name: product.name ?? "", attribute: product.shortDescription ?? "")
         } else {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: verticalCellIdentifier, for: indexPath) as! ProductCell
-            
-            let currentProduct = mainProducts[indexPath.row]
-            
-            // Convert the imageURL string to URL
-            if let imageURL = URL(string: currentProduct.imageURL ?? "") {
-                // Configure the cell with Kingfisher
-                cell.configure(with: imageURL, price: currentProduct.priceText ?? "", name: currentProduct.name ?? "", attribute: currentProduct.attribute ?? "")
-            } else {
-                // Set a placeholder image if the imageURL string is invalid
-                cell.configure(with: nil, price: currentProduct.priceText ?? "", name: currentProduct.name ?? "", attribute: currentProduct.attribute ?? "")
-            }
-            
-            
-            return cell
+            cell.configure(with: nil, price: product.priceText ?? "", name: product.name ?? "", attribute: product.shortDescription ?? "")
+        }
+    }
+    
+    private func configureCellProductElement(_ cell: ProductCell, with product: ProductElement) {
+        if let imageURL = URL(string: product.imageURL ?? "") {
+            cell.configure(with: imageURL, price: product.priceText ?? "", name: product.name ?? "", attribute: product.attribute ?? "")
+        } else {
+            cell.configure(with: nil, price: product.priceText ?? "", name: product.name ?? "", attribute: product.attribute ?? "")
         }
     }
 }
 
-extension ProductListingViewController: UICollectionViewDelegate {
+class BackgroundDecorationView: UICollectionReusableView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white // Set the desired background color here
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
